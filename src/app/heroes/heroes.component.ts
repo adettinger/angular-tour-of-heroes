e12c5d1fc54e7d96f011b0f0a9e3975d1fe9fcc8
adettinger <apddeapp@gmail.com>
2025-11-12 15:32:03 +0100

    Adding services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   standalone: true,
   selector: 'app-heroes',
   imports: [
-    FormsModule,
     NgFor,
     HeroDetailComponent
 ],
@@ -17,10 +17,21 @@ import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
-
   selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
