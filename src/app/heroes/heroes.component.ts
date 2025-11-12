75ced4c9c849d002a255905ef5ba89f9eb14421e
adettinger <apddeapp@gmail.com>
2025-11-12 14:51:54 +0100

    Through part 3

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+
+@Component({
+  standalone: true,
+  selector: 'app-heroes',
+  imports: [
+    FormsModule,
+    NgFor,
+    HeroDetailComponent
+],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
